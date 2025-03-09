044edd1e96f40b10bfa08759f85e8498c7bc04af
Kesaba Chandra Barik <166729378+KESABA-BARIK@users.noreply.github.com>
2025-03-09 21:58:46 +0530

    Create 1	Life, the Universe, and Everything.cpp

---
+++ 1	Life, the Universe, and Everything.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	int n;
+	// your code here
+	while(true){
+		cin>>n;
+		if(n==42){
+			break;
+		}
+		cout<<n<<endl;
+	}
+ 
+	return 0;
+} 
